5d290f4b218f7f5d3f8be512301df750c306d6ad
TigorImamHamdani <2031710058@student.polinema.ac.id>
2023-02-21 10:34:42 +0700

    prak1

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name"          : "New Addoms",
+    "version"       : "1.0",
+    "author"        : "Tigor Imam Hamdani",
+    "website"       : "https://www.detik.com",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+        Latihan membuat addons baru di odoo 1 
+        """,
+    "depends"       : ["base",],
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
